115455cd6b1f4918e8f05fed3d562fe649759322
harry4455 <xoxolittlemonster4455@gmail.com>
2021-02-13 19:58:49 +0900

    Making Movie App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from 'react';
-class App extends React.Component{
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
+
+class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    console.log("add")
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState(current => ({count: current.count - 1}));
-  };
-  componentDidMount(){
-    console.log("component rendered");
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   }
-  componentDidUpdate(){
-    console.log("I just updated");
+
+  componentDidMount() {
+    this.getMovies();
   }
-  componentWillUnmount(){
-    console.log("Goodbye, cruel World!");
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (<div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image} 
+                  genres={movie.genres}
+                />
+              ))}  
+            </div>     
+          )};
+      </section>
+    );
   }
-  render(){
-    console.log("I'm rendering");
-    return <div>
-      <h1> The number is :  {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-  };
 }
 
 export default App;
